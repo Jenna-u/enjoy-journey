30cb15193dc4b43de81aa61d9f24cc0affd11a1e
shanyaxuan <shanyaxuan@innobuddy.com>
2017-11-11 16:45:44 +0800

    添加util.js

--- js/util.js
+++ js/util.js
@@ -2,3 +2,12 @@
 window.api = '';
 // cdn路径
 window.cdn = 'http://pic.lxqcheng.com/';
+// 一个比较参数是否等于某值的方法
+if(Handlebars) {
+  Handlebars.registerHelper("compare",function(v1, v2, opts){
+    if(v1 == v2)
+      return opts.fn(this);
+    else
+      return opts.inverse(this);
+  });
+}
