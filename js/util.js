fe90d10963615ecb8cac0d5a7f67c958953e69cf
shanyaxuan <shanyaxuan@innobuddy.com>
2017-11-12 21:06:50 +0800

    在util里写了个获取url上参数的方法

--- js/util.js
+++ js/util.js
@@ -1,5 +1,5 @@
 // api地址
-window.api = '';
+window.api = 'http://app.lxqcheng.com/';
 // cdn路径
 window.cdn = 'http://pic.lxqcheng.com/';
 // 一个比较参数是否等于某值的方法
@@ -11,3 +11,19 @@ if(Handlebars) {
       return opts.inverse(this);
   });
 }
+// 获取url上的参数
+function getParam() {
+  var url = location.search;
+  var theRequest = new Object();
+  if (url.indexOf("?") != -1) {
+    var str = url.substr(1);
+    var strs = str.split("&");
+    for(var i = 0; i < strs.length; i ++) {
+      theRequest[strs[i].split("=")[0]] = unescape(strs[i].split("=")[1]);
+    }
+  }
+  return theRequest;
+}
+
+window.urlParams = getParam();
+console.log('window.urlParams', window.urlParams)
