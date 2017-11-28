e74399e690d03e5460c7133e649f0b0dd08ba77d
shanyaxuan <shanyaxuan@innobuddy.com>
2017-11-28 19:24:03 +0800

    更新一些图片和样式文件

--- js/scenic_spot/order.js
+++ js/scenic_spot/order.js
@@ -23,9 +23,41 @@ if(window.urlParams.id) {
 
 /* 提交景点订单 */
 function submitOrder () {
+  if(!window.lxqc_user || !window.lxqc_user.id) {
+    location.href = "login.html?redirect=" + location.href;
+    return false;
+  } 
   var ticketId = window.urlParams.id;
+  var memberId = window.lxqc_user.id;
   var num = $('.Amount').val();
   var name = $('#name').val();
   var mobile = $('#mobile').val();
+  var data = '2017/11/24';
   var readProtocol = $('#xy').prop('checked');
+  if (!mobile || !/^0?(1)[0-9]{10}$/.test(mobile)) {
+    alert('亲，手机号码格式错误！')
+    return false
+  }
+  var data= {
+    ticketId: ticketId,
+    memberId: memberId,
+    num: num,
+    name: name,
+    mobile: mobile,
+    data: data
+  };
+  $.ajax({
+    url: window.api + "ticket/addOrder",
+    type: 'POST',
+    dataType: 'json',
+    data: data,
+    success: function (res) {
+      console.log(res,'ress')
+      if(res.code == 200) {
+        
+      } else {
+        alert(res.msg)
+      }
+    }
+  });
 }
